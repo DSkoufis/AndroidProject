c71d1f2c5004ca5f220d9b60f406d5119fd1ad40
SkoufisDimitrios <skoufisdimitris@gmail.com>
2016-12-19 18:42:44 +0200

    Some more changes

--- app/src/main/java/mobile/uom/gr/androidproject/FlightsActivity.java
+++ app/src/main/java/mobile/uom/gr/androidproject/FlightsActivity.java
@@ -8,6 +8,6 @@ public class FlightsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_flights);
+        setContentView( R.layout.activity_flights);
     }
 }
