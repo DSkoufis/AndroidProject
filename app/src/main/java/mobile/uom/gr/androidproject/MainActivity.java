17b6b9ddc60d78510d59fb969e69fa0ee2edb425
SkoufisDimitrios <skoufisdimitris@gmail.com>
2016-12-20 17:00:02 +0200

    Hid API calls as said in class

--- app/src/main/java/mobile/uom/gr/androidproject/MainActivity.java
+++ app/src/main/java/mobile/uom/gr/androidproject/MainActivity.java
@@ -1,17 +1,24 @@
 package mobile.uom.gr.androidproject;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 
     public void searchFlights(View view) {
         Intent intent = new Intent(this, SearchFlightsActivity.class);
