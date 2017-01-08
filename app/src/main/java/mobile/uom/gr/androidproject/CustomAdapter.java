250f7b0405729c8ea3f9b350622e5caf6fcabd73
SkoufisDimitrios <skoufisdimitris@gmail.com>
2017-01-08 20:25:35 +0200

    added a class

---
+++ app/src/main/java/mobile/uom/gr/androidproject/CustomAdapter.java
@@ -0,0 +1,33 @@
+package mobile.uom.gr.androidproject;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ *  Custom Adapter that help us put the results from API call for low-fare flights
+ *  and put them in the ListView in activity_flights.xml
+ */
+
+public class CustomAdapter extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
